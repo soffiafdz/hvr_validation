5b0f61fa4e9f0d5b01ab7a8caacc98bece362b38
sfernandez <sofia.fernandezlozano@mail.mcgill.ca>
2025-02-03 11:35:16 -0500

    Use GNU parallel

--- code/backend/compare_man-seg.sh
+++ code/backend/compare_man-seg.sh
@@ -4,11 +4,6 @@
 ## { MALF, NLPB, CNN } — manual labels
 ## XCorrelation
 
-TMPDIR=$(mktemp -d --tmpdir)
-trap "rm -rf $TMPDIR" 0 1 2 15
-
-set -ux
-
 ## FUNCTIONS
 # Recode labels to L-HC: 1 & R-HC: 2 & L-VC: 3 & R-VC: 4
 # Reshape to Dorothee's labels
@@ -51,59 +46,52 @@ compare() {
 	}'
 }
 
-# PATHS
+# Process
+label_comparison() {
+	local man_lab="$1"
+	local TMPDIR=$(mktemp -d --tmpdir)
+	trap "rm -rf $TMPDIR" EXIT
+
+	# Extract id
+	local id=$(echo $man_lab | grep -oP "0\d{2}")
+
+	# Reshape manual label
+	local man_lab_=${TMPDIR}/${id}_man_recoded.mnc
+	recode "$man_lab" "$man_lab" "$man_lab_"
+
+	# Process each segmentation method
+	for segm in malf nlpb cnn
+	do
+		local path_var="${segm^^}_DIR"
+		local seg_lab=$(find "${!path_var}" -name "*${id}*")
+		local seg_lab_="${TMPDIR}/${id}_${segm}_reshaped.mnc"
+		recode "$seg_lab" "$man_lab_" "$seg_lab_"
+		compare "$id" "$man_lab_" "$seg_lab_" >> "${OUTFILE/method/$segm}"
+	done
+}
+
+# Export functions
+export -f recode
+export -f compare
+export -f label_comparison
+
+# CONSTANTS
 HERE=/path/to/user/project/hvr_validation
-MAN_LABS=${HERE}/data/labels_dorothee/labels_reduced
-MALF_LABS=${HERE}/data/orig_segmentations/malf/reduced
-NLPB_LABS=${HERE}/data/orig_segmentations/nlpb/reduced
-CNN_LABS=${HERE}/data/orig_segmentations/cnn/hcvc
+MAN_DIR=${HERE}/data/labels_dorothee/labels_reduced
+MALF_DIR=${HERE}/data/orig_segmentations/malf/reduced
+NLPB_DIR=${HERE}/data/orig_segmentations/nlpb/reduced
+CNN_DIR=${HERE}/data/orig_segmentations/cnn/hcvc
 OUTFILE=${HERE}/data/derivatives/man-seg_kappa_hcvc_method.csv
 
+# Export constants
+export MALF_DIR NLPB_DIR CNN_DIR OUTFILE
+
+# INIT csv files
 CSV_HEADER="id,roi,side,dice,kappa,accuracy,sensitivity,specificity"
 for segm in malf nlpb cnn
 do
-	echo $CSV_HEADER > ${OUTFILE/method/$method}
+	echo $CSV_HEADER > ${OUTFILE/method/$segm}
 done
 
-for manual_lab in ${MAN_LABS}/*
-do
-	# Extract id
-	id=$(echo $manual_lab | grep -oP "0\d{2}")
-
-	# Reshape manual label
-	man_=${TMPDIR}/${id}_man_recoded.mnc
-	recode "$manual_lab" "$manual_lab" "$man_"
-
-	# MALF
-	# Find corresponding segmented labels
-	malf=$(find $MALF_LABS -name "*$id*")
-
-	# Recode & reshape
-	malf_=${TMPDIR}/${id}_malf_reshaped.mnc
-	recode "$malf"  "$manual_lab" "$malf_"
-
-	# Comparison
-	compare $id $man_ $malf_ >> ${OUTFILE/method/malf}
-
-	# NLPB
-	# Find corresponding segmented labels
-	nlpb=$(find $NLPB_LABS -name "*$id*")
-
-	# Recode & reshape
-	nlpb_=${TMPDIR}/${id}_nlpb_reshaped.mnc
-	recode "$nlpb"  "$manual_lab" "$nlpb_"
-
-	# Comparison
-	compare $id $man_ $nlpb_ >> ${OUTFILE/method/nlpb}
-
-	# CNN
-	# Find corresponding segmented labels
-	cnn=$(find $CNN_LABS -name "*$id*")
-
-	# Recode & reshape
-	cnn_=${TMPDIR}/${id}_cnn_reshaped.mnc
-	recode "$cnn"  "$manual_lab" "$cnn_"
-
-	# Comparison
-	compare $id $man_ $cnn_ >> ${OUTFILE/method/cnn}
-done
+## Parallel comparison
+find "${MAN_DIR}" -type f | parallel --jobs 6 label_comparison
